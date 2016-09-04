b7fa797a87732b863dd0ec7da87cd12ed61f47a3
Patricio Soriano <pasoriano@gmail.com>
2016-09-04 13:59:08 +0200

    Completando parámetros obligatorio y carpeta con csv de ejemplos. Readme por terminar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wms2image
 
-Script de Python para obtener imágenes de un listado de servicios de visualización de mapas (WMS)
+Script de Python para obtener un conjunto de imágenes (Getmap) de un listado de servicios WMS de un rectángulo geográfico definido mediante par de coordendas en un EPSG UTM.
 
 <iframe src="//giphy.com/embed/l2SqaIg0lAiLIrMYM" width="480" height="495" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/spain-cordoba-wms-l2SqaIg0lAiLIrMYM">via GIPHY</a></p>
 
@@ -15,18 +15,63 @@ Ejecutar
 
     pip install -r requirements.txt
 
+## Instalación
+
+git clone
+descarga zip
 
 ## Uso
 
-    python wms2image.py 800 "332401,4188300,341225,4193019" jpeg
+    python wms2image.py wmslist/wmslist.csv 25830 "332401,4188300,341225,4193019" 800 tiff
 
-- Debe existir el archivo wmslist.csv en el mismo directorio que el archivo py
+    import wms2image
+    wms2image.wms2image('wmslist.csv, '25830','332401,4188300,341225,4193019','800','png')
+
+
+## Parámetros
+
+Todos los parámetros son obligatorio
+
+- csvfile: Archivo en formato CSV con el listado de servicios WMS
+- code: Sistema de Referencia de Coordenadas del DXF (código EPSG)
+- bbox: Par de coordendas del área geográfica
+- imgwidth: Ancho de la imagen
+- imgformat: Formato de la imagen
+
 - Las coordenadas serán UTM
-- EPGS es 25830
-- Formatos para la solicitud de getmap solo de tipo imágen.
+    + ETR89 UTM 28 al 31 (25828, 25829, 25830 y 25831)
+    + ED50 UTM 28 al 31 (23028, 23029, 23030 y 23031)
+
+- Formatos para la solicitud de getmap solo de tipo imágen. Nota: Cada WMS soporta un listado de formatos específicos.
+
     + png
     + tiff
     + jpeg
     + png
+    + gif
 
-# Editar archivo wmslist.csv
+# Crear nuevo archivo de WMS
+
+Para crear un listado de capas de WMS a usar podemos usar cualquier editor de texto. La estructura del archivo es sencilla. Para capa servicio se añadirá una file en el archivo que contenga la url del WMS y a continuación separado por como el nombre de la capa y por último el nombre con el que se guardará el fichero de image.
+
+Ej.
+    http://www.ign.es/wms/pnoa-historico,PNOA2004,2004_pnoa
+    http://www.ign.es/wms/pnoa-historico,PNOA2005,2005_pnoa
+    http://www.ign.es/wms/pnoa-historico,PNOA2006,2006_pnoa
+    http://www.ign.es/wms/pnoa-historico,PNOA2007,2007_pnoa
+
+Para obtener el listado de capas de un WMS puede consultarse los metadatos del servicio a través de GetCapabilities
+
+Ej. [http://www.ign.es/wms/pnoa-historico?request=GetCapabilities&service=WMS](http://www.ign.es/wms/pnoa-historico?request=GetCapabilities&service=WMS)
+
+# Obtener coordenadas
+
+Por ahora el script sólo puede usarse con sistema de referencia de coordenadas UTM. Esto se debe a que las coordendas métricas son utilizadas, junto al ancho de imagen definido para calcular la altura de la imagen final
+
+Las coordendas se debe indicar en el siguiente order:
+    - 1º x e y de esquina
+    - 2º x e y de esquina
+
+IMG
+
+Una forma rápida de obtener estas coordendas en por ejemplo utilizar el SIG QGIS. 
--- wms2image.py
+++ wms2image.py
@@ -1,18 +1,37 @@
 # !/usr/bin/python  
-# -*- coding: utf-8 -*-   
+# -*- coding: utf-8 -*- 
+
+__author__ = [
+    'Patricio Soriano Castro <pasoriano@gmail.com>'
+]
+__license__ = 'GPLv3'
+
 """Example Google style docstrings.
 
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
+Descripción
+===========
+El script genera un fichero GML de parcela catastral según las especificaciones de Castastro.
+Basado en https://github.com/sigdeletras/dxf2gmlcatastro (Patricio Soriano :: SIGdeletras.com)
 
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
+Especificaciones
+================
+    * http://www.catastro.minhap.gob.es/esp/formatos_intercambio.asp
+    * Cada parcela debe estar en una capa en cuyo nombre se establecerá su referencia.
+    * No se permiten incorporar en el mismo fichero dxf parcelas con referencias catastrales y referencias locales mezcladas, todas deben ser del mismo tipo, o bien locales o bien catastrales.
+    * Se asume referencia catastral si la longitud de la referencia es de 14 caracteres.
+    * Las geometrías deben ser sólidas y estar cerradas (el primer y último punto del polígono deben ser el mismo)
+Requisitos
+==========
+Es necesario tener instalado Python y el módulo GDAL (python-gdal).
 
-        $ python example_google.py
-
+Ejemplos de uso
+===============
+    * python dxfgmlcatastro.py <parcela1.dxf>
+         generará el fichero parcela1.gml
+    * python dxfgmlcatastro.py <parcela1.dxf> 25831
+         generará el fichero parcela1.gml usando el código EPSG 25831
+    * python dxfgmlcatastro.py <mi_directorio>
+         generará un fichero .gml por cada fichero .dxf que se encuentre en mi_directorio
 
 """
 
@@ -22,9 +41,60 @@ import os
 import sys
 import requests
 
+try:
+    import requests
+except ImportError:
+    print('Error: requests no instalado')
+    sys.exit(1)
 
-def wms2image(imgwidth, bbox, imgformat):
-    wmslist = csv.reader(open("wmslist.csv", "rt"), delimiter=',')
+# Sistemas de referencia de coordenadas
+EPSG_ZONES = [
+    '25828',  # ETRS89 / UTM zone 28N
+    '25829',  # ETRS89 / UTM zone 29N
+    '25830',  # ETRS89 / UTM zone 30N
+    '25831',  # ETRS89 / UTM zone 31N
+    '23028',  # ED50 / UTM zone 28N
+    '23029',  # ED50 / UTM zone 29N
+    '23030',  # ED50 / UTM zone 30N
+    '23031',  # ED50 / UTM zone 31N
+]
+
+
+# Formatos de imágen
+IMAGE_FORMAT = [
+    'png',  
+    'jpeg',  
+    'tiff',  
+    'gif',  
+]
+
+
+def wms2image(csvfile, code, bbox, imgwidth, imgformat):
+    """
+    Obtiene un conjunto de imágenes (Getmap) de un listado de servicios
+    WMS de un rectángulo geográfico definido mediante par de coordendas
+    en un EPSG UTM.
+    csvfile: Archivo en formato CSV con el listado de servicios WMS
+    code: Sistema de Referencia de Coordenadas del DXF (código EPSG)
+    bbox: Par de coordendas del área geográfica
+    imgwidth: Ancho de la imagen
+    imgformat: Formato de la imagen
+    """
+    if code not in EPSG_ZONES:
+        return (
+            False,
+            u'Error: El código EPSG "%s" es incorrecto' % code
+        )
+        sys.exit(1)        
+    
+    if imgformat not in IMAGE_FORMAT:
+        return (
+            False,
+            u'Error: El formato "%s" es incorrecto' % imgformat
+        )
+        sys.exit(1)    
+
+    wmslist = csv.reader(open(csvfile, "rt"), delimiter=',')
 
     bboxcoord = ast.literal_eval(bbox)
     imgheight = ((bboxcoord[3] - bboxcoord[1]) * int(imgwidth)) / (bboxcoord[2] - bboxcoord[0])
@@ -33,7 +103,7 @@ def wms2image(imgwidth, bbox, imgformat):
         wms = row[0]
         layer = row[1]
         file = row[2]
-        url = wms + '?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&LAYERS=' + layer + '&STYLES=&SRS=EPSG:25830&BBOX=' \
+        url = wms + '?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&LAYERS=' + layer + '&STYLES=&SRS=EPSG:' + str(code) + '&BBOX=' \
               + bbox + '&WIDTH=' + str(imgwidth) + '&HEIGHT=' + str(imgheight) + '&FORMAT=image/' + imgformat
 
         r = requests.get(url)
@@ -42,9 +112,50 @@ def wms2image(imgwidth, bbox, imgformat):
 
         print("Downloading..." + file + "." + imgformat)
 
-if __name__ == '__main__':
-    imgwidth = sys.argv[1]
-    bbox = sys.argv[2]
-    imgformat = sys.argv[3]
 
-    wms2image(imgwidth, bbox, imgformat)
+def usage():
+    print(u'\nParámetros:')
+
+    print(u'\t1. csvfile: nombre del archivo csv con el listado de servicios WMS')
+    print(u'\t2. code: Código EPGS del Sistema de referencia de coordenadas. Solo UTM.')
+    print(u'\t3. bbox: Pares de coordenadas de las esquinas de área. Solo UTM')
+    print(u'\t4. imgwidth: Ancho de la imagen')
+    print(u'\t5. imgformat: Formato de la imagen')
+
+    print(u'\nEjemplos de uso:')
+
+    print(u'\tObtiene un conjunto de imágenes en formato png del listado pnoa.csv:\n')
+    
+    print(u'\t$ python wms2image.py pnoa.csv 800 25830 "332401,4188300,341225,4193019" png')
+
+
+def main():
+    if len(sys.argv) < 6:
+        print(u'Error: parámetros insuficientes')
+        usage()
+        sys.exit(1)
+    
+    csvfile = sys.argv[1]
+    code = sys.argv[2]
+    bbox = sys.argv[3]
+    imgwidth = sys.argv[4]
+    imgformat = sys.argv[5]
+
+    if sys.argv[2] not in EPSG_ZONES:
+        return (
+            False,
+            print(u'Error: El código EPSG "%s" es incorrecto' % sys.argv[2])
+        )
+        sys.exit(1)        
+    
+    if sys.argv[5] not in IMAGE_FORMAT:
+        return (
+            False,
+            print(u'Error: El formato "%s" es incorrecto' % sys.argv[5])
+        )
+        sys.exit(1)
+
+    wms2image(csvfile, code, bbox, imgwidth, imgformat)
+
+if __name__ == '__main__':
+    main()
